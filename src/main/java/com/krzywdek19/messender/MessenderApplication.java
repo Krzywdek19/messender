73a2a66aab5da8d480c71a8458da019b37326002
krzywdek19 <krzywdun@wp.pl>
2024-06-19 12:01:23 +0200

    Init springboot application

---
+++ src/main/java/com/krzywdek19/messender/MessenderApplication.java
@@ -0,0 +1,13 @@
+package com.krzywdek19.messender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krzywdek19/messender/MessenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzywdek19.messender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
